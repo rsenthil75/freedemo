150dcc6cebb1b06acd9d264d73d34940c71a3096
java <java@localhost>
2021-08-10 18:33:43 -0500

    Adding log4j feature.

--- SeleniumDemo/src/main/java/com/selenium/demo/TableFunction.java
+++ SeleniumDemo/src/main/java/com/selenium/demo/TableFunction.java
@@ -2,6 +2,8 @@ package com.selenium.demo;
 
 import java.util.List;
 
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -11,6 +13,12 @@ public class TableFunction {
 
 	public static void main(String[] args) {
 		
+		Logger log=Logger.getLogger(TableFunction.class);
+		
+		org.apache.log4j.BasicConfigurator.configure();
+		
+		
+		
 		WebDriver driver = new ChromeDriver();
 		driver.get("file:///Users/java/dev/freedemo/SeleniumDemo/src/main/WebApp/WebAppForTest.html");
 
@@ -18,22 +26,22 @@ public class TableFunction {
 
 		WebElement dataElement = tableElement.findElements(By.tagName("td")).get(4);
 		
-		System.out.println("Fetched Table data by using Tag Name: " + dataElement.getText());
+		log.info("Fetched Table data by using Tag Name: " + dataElement.getText());
 
 		// Fetch the table element by using xpath
 
 		WebElement dataElement2 = driver.findElement(By.xpath("/html/body/form/table/tbody/tr[3]/td[1]"));
 
-		System.out.println("Fetched Table data by using XPATH: " + dataElement2.getText());
+		log.info("Fetched Table data by using XPATH: " + dataElement2.getText());
 		
 		
 		//Fetch and Print all the elements from the <TD>
-		System.out.println("Fatching all the elements from TD");
+		log.info("Fatching all the elements from TD");
 		List<WebElement> dataElements = tableElement.findElements(By.tagName("td"));
 		
 		for (WebElement we :  dataElements) {
 			
-			System.out.println("Table Data Element Tag: "+we.getTagName()+" Text="+we.getText());
+			log.info("Table Data Element Tag: "+we.getTagName()+" Text="+we.getText());
 		}
 		
 		driver.close();
--- SeleniumDemo/src/main/java/com/selenium/demo/TestCheckBox.java
+++ SeleniumDemo/src/main/java/com/selenium/demo/TestCheckBox.java
@@ -1,5 +1,6 @@
 package com.selenium.demo;
 
+import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -8,6 +9,13 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class TestCheckBox {
 
 	public static void main(String[] args) {
+		
+		Logger log=Logger.getLogger(TestCheckBox.class);
+	
+	org.apache.log4j.BasicConfigurator.configure();
+	
+		
+		
 		WebDriver driver = new ChromeDriver();
 		driver.get(
 				"file:///Users/java/dev/freedemo/SeleniumDemo/src/main/WebApp/WebAppForTest.html");
@@ -15,7 +23,7 @@ public class TestCheckBox {
 		WebElement chkbx = driver.findElement(By.id("rkwd"));
 		chkbx.click();
 
-		System.out.println(chkbx.isSelected());
+		log.info("Checkbox Selection: "+chkbx.isSelected());
 
 
 	}

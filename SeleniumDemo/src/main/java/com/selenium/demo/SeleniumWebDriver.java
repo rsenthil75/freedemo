b5c9a2dee782443a4a1aef92f4db99f7fb8364ba
rsenthil75 <rsenthil75@hotmail.com>
2021-09-21 17:21:26 -0500

    Update SeleniumWebDriver.java

--- SeleniumDemo/src/main/java/com/selenium/demo/SeleniumWebDriver.java
+++ SeleniumDemo/src/main/java/com/selenium/demo/SeleniumWebDriver.java
@@ -8,7 +8,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class SeleniumWebDriver {
 
 	public static void main(String[] args) {
-
+	
+		System.out.println("In Main Method");
 		
 		WebDriver driver = new ChromeDriver();
 
